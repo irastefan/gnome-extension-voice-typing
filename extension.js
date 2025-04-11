b63d3ba2c70615567082cbf751b8dbf6032ec4b7
Ira Stefan <ira.stefan91@gmail.com>
2025-04-11 17:07:58 +0300

    fixed bugs

--- extension.js
+++ extension.js
@@ -1,3 +1,4 @@
+// Updated GNOME extension: debug notify, safe readStatus, log subprocess
 import St from 'gi://St';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
@@ -12,9 +13,14 @@ let checkStatusLoop;
 function readStatus() {
     try {
         if (!GLib.file_test(STATUS_FILE, GLib.FileTest.EXISTS)) return 'idle';
-        return GLib.file_get_contents(STATUS_FILE)[1].toString().trim();
+        let [ok, contents] = GLib.file_get_contents(STATUS_FILE);
+        if (ok && contents instanceof Uint8Array) {
+            const decoder = new TextDecoder();
+            return decoder.decode(contents).trim();
+        }
+        return 'idle';
     } catch (e) {
-        log('Voice Typing: Failed to read status file: ' + e);
+        log('[Voice Typing] Failed to read status file: ' + e);
         return 'idle';
     }
 }
@@ -22,7 +28,7 @@ function readStatus() {
 function updateIcon() {
     const status = readStatus();
     const iconName = status === 'recording'
-        ? 'microphone-sensitivity-medium-symbolic' 
+        ? 'microphone-sensitivity-medium-symbolic'
         : 'microphone-sensitivity-high-symbolic';
     icon.icon_name = iconName;
 }
@@ -38,20 +44,25 @@ export default class Extension {
 
         indicator.add_child(icon);
 
-        const startItem = new PopupMenu.PopupMenuItem('Start Recording');
-        startItem.connect('activate', () => {
-            GLib.spawn_command_line_async('python3 voice_typing_toggle.py');
-        });
+        // Start Recording (with play icon)
+        const startItem = new PopupMenu.PopupImageMenuItem('Start Recording', 'media-playback-start-symbolic');
+         startItem.connect('activate', () => {
+             GLib.spawn_command_line_async('python3 /home/irastefan/Documents/projects/whisper-fedora/voice_typing_toggle.py');
+         });
         indicator.menu.addMenuItem(startItem);
 
-        const stopItem = new PopupMenu.PopupMenuItem('Stop Recording');
+        // Stop Recording (with stop icon)
+        const stopItem = new PopupMenu.PopupImageMenuItem('Stop Recording', 'media-playback-stop-symbolic');
         stopItem.connect('activate', () => {
+            log('[Voice Typing] Stop flag set');
             GLib.file_set_contents('/tmp/voice_typing_stop.flag', 'stop');
         });
         indicator.menu.addMenuItem(stopItem);
 
-        const forceStopItem = new PopupMenu.PopupMenuItem('Force Stop (Kill)');
+        // Force Stop (Kill) (with exit icon)
+        const forceStopItem = new PopupMenu.PopupImageMenuItem('Force Stop (Kill)', 'application-exit-symbolic');
         forceStopItem.connect('activate', () => {
+            log('[Voice Typing] Sending pkill to script');
             GLib.spawn_command_line_async('pkill -f voice_typing_toggle.py');
         });
         indicator.menu.addMenuItem(forceStopItem);
@@ -75,4 +86,3 @@ export default class Extension {
         }
     }
 }
-
